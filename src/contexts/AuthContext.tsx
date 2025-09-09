ffc741e9dae8dbfea8aaa37425d29aba63ceaa77
wn.kim <unkim@x-alliance.co>
2025-09-09 14:56:26 +0900

    LocalStorage -> Cookie 로변경

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 // AuthContextType 인터페이스와 AuthContext를 컴포넌트 외부로 이동시킵니다.
 // 이렇게 하면 Fast Refresh 시 매번 새로운 컨텍스트가 생성되는 것을 방지할 수 있습니다.
@@ -22,15 +23,11 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     // 페이지 로드 시 로컬 스토리지에서 로그인 상태 확인
-    const token = getItemWithExpiry('token');
-    const savedEmail = localStorage.getItem('userEmail');
-    const savedDisplayName = localStorage.getItem('userDisplayName');
+    const token = Cookies.get('token') || null;
 
     if (token) {
       setIsLoggedIn(true);
       setToken(token);
-      setUserEmail(savedEmail);
-      setUserDisplayName(savedDisplayName);
     } else {
       console.log('유효하지 않은 토큰입니다.');
       logout();
@@ -42,10 +39,7 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
     setToken(token);
     setUserEmail(email);
     setUserDisplayName(displayName);
-    setItemWithExpiry(token);
-    localStorage.setItem('userEmail', email);
-    localStorage.setItem('userDisplayName', displayName);
-    localStorage.setItem('isLoggedIn', 'true');
+    Cookies.set('token', token, { expires: 1/24, path: "/", sameSite: "strict", domain: "127.0.0.1" });
   };
 
   const logout = () => {
@@ -53,39 +47,7 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
     setToken(null);
     setUserEmail(null);
     setUserDisplayName(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('userEmail');
-    localStorage.removeItem('userDisplayName');
-    localStorage.removeItem('isLoggedIn');
-  };
-
-  const setItemWithExpiry = (value: any) => {
-    const now = new Date();
-    const item = {
-      value: value,
-      expiry: now.getTime() + 3600 * 1000, // 만료 시간을 밀리초 단위로 계산하여 저장(초단위)
-    };
-    localStorage.setItem('token', JSON.stringify(item));
-  };
-
-  const getItemWithExpiry = (key: any) => {
-
-    // 저장된 아이템이 없으면 null 반환
-    const itemStr = localStorage.getItem(key);
-    if (!itemStr) {
-      return null;
-    }
-
-    const item = JSON.parse(itemStr);
-    const now = new Date();
-
-    // 현재 시간이 만료 시간보다 크면(만료되었으면)
-    if (now.getTime() > item.expiry) {
-      localStorage.removeItem(key);
-      return null;
-    }
-
-    return item.value;
+    Cookies.remove('token', { path: "/", sameSite: "strict", domain: "127.0.0.1" });
   };
 
   return (
